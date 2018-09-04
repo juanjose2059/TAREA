9afa84f0d713fe7894361ad53e46a1459c596764
Adriana Barcena Garcia <adriana.garcia.sc@hotmail.com>
2018-09-04 15:02:27 -0500

    Primer saludo de adriana

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -6,6 +6,7 @@ public class Tarea {
 
     public static void main(String[] args) {
        System.out.println("Hola grupo, ");
+       System.out.println("Hola buenas tardes de parte de adriana");
     }
     
 }
