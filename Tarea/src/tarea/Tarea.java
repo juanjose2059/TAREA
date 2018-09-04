f2a4a13bf4ef9c5a6ff97d589cda0776b61c8f24
Adriana Barcena Garcia <adriana.garcia.sc@hotmail.com>
2018-09-04 15:05:39 -0500

    despedida de adriana

--- Tarea/src/tarea/Tarea.java
+++ Tarea/src/tarea/Tarea.java
@@ -8,6 +8,7 @@ public class Tarea {
        System.out.println("Hola grupo, ");
        System.out.println("Hola buenas tardes de parte de adriana");
        System.out.println("Segundo saludo");
+       System.out.println("Adios,bye bye");
     }
     
 }
